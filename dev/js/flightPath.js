af46a4a11e3f5cfd3c69cd8dc656a8e1fa66f49b
Yerin Koo <yk459@drexel.edu>
2021-05-09 16:11:35 -0400

    update scenario after completed

--- dev/js/flightPath.js
+++ dev/js/flightPath.js
@@ -4,6 +4,9 @@ import { MotionPathHelper } from "gsap/MotionPathHelper";
 
 gsap.registerPlugin(MotionPathPlugin, MotionPathHelper);
 
+gsap.set("#moon",{transformOrigin:"center bottom"});
+
+
 export let flightTL = gsap.timeline();
 
 flightTL.to("#space-ship", {
@@ -17,10 +20,10 @@ flightTL.to("#space-ship", {
 })
 .to("#moon",{alpha:1, duration:2, x:"+=530", y:"-=640", scale: 5, ease: "none"},"-=13")
 .to(".flames",{opacity: 0}, "-=8")
-
-// .to("#space-ship",{duration:2.7, y:"-=1200", x:"-=100", scale: 0.6}, 'upAndAway', "-=6.5")
-// .to(".flames",{opacity: 1}, "-=6")
-// .to("#moon",{opacity:"0"}, "-=1.5")
+.to("#space-ship",{duration:2.7, y:"-=1200", x:"-=100", scale: 0.6}, "-=6.2")
+.to(".flames",{opacity: 1}, "-=6.2")
+.to("#space-ship",{opacity:"0"}, "-=5")
+.to("#moon",{opacity:"0"}, "-=1.5")
 
 
 
