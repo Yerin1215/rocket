6df759b2cba0c74a33762f617bd4aaf862083043
Yerin Koo <yk459@drexel.edu>
2021-05-09 14:03:03 -0400

    .
    .

--- dev/js/moonZoom.js
+++ dev/js/moonZoom.js
@@ -1,5 +1,5 @@
 import { gsap } from "gsap";
-gsap.set("#moon",{transformOrigin:"center bottom"});
+// gsap.set("#moon",{transformOrigin:"center bottom"});
 
 export let moonZoomTL = gsap.timeline();
 // moonZoomTL.to("#moon",{alpha:1, duration:2, x:"+=530", y:"-=640", scale: 5, ease: "none"},"-=15")
