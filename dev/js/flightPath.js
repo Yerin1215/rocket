41b2cc6645ced9d00124bdb3927cbac1b9f3921e
Yerin Koo <yk459@drexel.edu>
2021-05-09 15:12:41 -0400

    .

--- dev/js/flightPath.js
+++ dev/js/flightPath.js
@@ -17,8 +17,8 @@ flightTL.to("#space-ship", {
 })
 .to("#moon",{alpha:1, duration:2, x:"+=530", y:"-=640", scale: 5, ease: "none"},"-=13")
 .to(".flames",{opacity: 0}, "-=8")
-.to("#space-ship",{opacity:"0"}, "-=3.5")
-.to("#moon",{duration:3.5, y:"-=1200", x:"-=178", scale: 0.6}, "-=1.5")
+.to("#space-ship",{duration:2.7, y:"-=1200", x:"-=100", scale: 0.6}, 'upAndAway', "-=3.5")
+.to("#moon",{opacity:"0"}, "-=1.5")
 
 
 
